becbe58696b2f669c75aad6fd944cc32ea7adf39
David Billups <davidtbillups@gmail.com>
2021-01-21 11:16:28 -0500

    completed sowing-the-field module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,17 @@ console.log("Welcome to the main module")
 import { createPlan } from './plan.js'
 import { createAsparagus } from './seeds/asparagus.js'
 import { addPlant , usePlants } from './field.js'
+import { plantSeeds } from './tractor.js'
+import { createCorn } from './seeds/corn.js'
 
 const yearlyPlan = createPlan();
 console.log(yearlyPlan)
 
-let seed = createAsparagus();
+// let seed = createCorn();
+// console.log(seed)
 
-addPlant(seed);
+// addPlant(seed);
+// console.log(usePlants())
+
+plantSeeds(yearlyPlan);
 console.log(usePlants())
